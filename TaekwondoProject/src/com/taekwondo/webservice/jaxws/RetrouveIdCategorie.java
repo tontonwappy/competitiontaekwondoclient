dcec138e6a8c715fc0e1f0245d0a5502cb4768fc
tontonwappy <tontonwappy@hotmail.com>
2014-03-27 14:14:40 +0100

    comit

---
+++ TaekwondoProject/src/com/taekwondo/webservice/jaxws/RetrouveIdCategorie.java
@@ -0,0 +1,36 @@
+
+package com.taekwondo.webservice.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "retrouveIdCategorie", namespace = "http://webservice.taekwondo.com/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "retrouveIdCategorie", namespace = "http://webservice.taekwondo.com/")
+public class RetrouveIdCategorie {
+
+    @XmlElement(name = "arg0", namespace = "")
+    private String arg0;
+
+    /**
+     * 
+     * @return
+     *     returns String
+     */
+    public String getArg0() {
+        return this.arg0;
+    }
+
+    /**
+     * 
+     * @param arg0
+     *     the value for the arg0 property
+     */
+    public void setArg0(String arg0) {
+        this.arg0 = arg0;
+    }
+
+}
